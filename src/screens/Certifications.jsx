4defa07bd6d830acb3bf34b13c6f3f3ffa102a45
Agos <cuentasecundariaagosss@gmail.com>
2023-03-20 19:31:37 -0300

    second

--- src/screens/Certifications.jsx
+++ src/screens/Certifications.jsx
@@ -4,7 +4,7 @@ import { FaFreeCodeCamp } from "react-icons/fa";
 const Certifications = () => {
   return (
     <main className='main'>
-        <h1>Certificacions</h1>
+        <h1>Certifications</h1>
         <section className='certification__container'>
           <div className='certification__item'>
             <div className='certification__icon-ct'>
@@ -12,7 +12,7 @@ const Certifications = () => {
             </div>
             <div className='certification__item-info'>
               <span className='certification__item-info-ttl'>Diseño web adaptativo</span>
-              <a target='_blank' href='https://www.freecodecamp.org/certification/fcc6f507e86-2390-4483-8447-505a4a0db0a6/javascript-algorithms-and-data-structures'>Ver certificacion</a>
+              <a className='link' target='_blank' href='https://www.freecodecamp.org/certification/fcc6f507e86-2390-4483-8447-505a4a0db0a6/javascript-algorithms-and-data-structures'>Ver certificacion</a>
               
             </div>
           </div>
@@ -22,7 +22,7 @@ const Certifications = () => {
             </div>
             <div className='certification__item-info'>
               <span className='certification__item-info-ttl'>Javascript Algorithms and Data Structures</span>
-              <a target='_blank' href='https://www.freecodecamp.org/certification/fcc6f507e86-2390-4483-8447-505a4a0db0a6/javascript-algorithms-and-data-structures'>Ver certificacion</a>
+              <a className='link' target='_blank' href='https://www.freecodecamp.org/certification/fcc6f507e86-2390-4483-8447-505a4a0db0a6/javascript-algorithms-and-data-structures'>Ver certificacion</a>
               
             </div>
           </div>
@@ -32,7 +32,7 @@ const Certifications = () => {
             </div>
             <div className='certification__item-info'>
               <span className='certification__item-info-ttl'>Back End Developement and APIs</span>
-              <a target='_blank' href='https://www.freecodecamp.org/certification/fcc6f507e86-2390-4483-8447-505a4a0db0a6/back-end-development-and-apis'>ver certificacion</a>
+              <a className='link' target='_blank' href='https://www.freecodecamp.org/certification/fcc6f507e86-2390-4483-8447-505a4a0db0a6/back-end-development-and-apis'>ver certificacion</a>
             </div>
           </div>
         </section>
--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { SiJavascript,SiReact,SiSocketdotio } from "react-icons/si";
+import { BsGithub } from "react-icons/bs";
 
 const Projects = () => {
   return (
-    <main>
+    <main className='main'>
       <h1>Projects</h1>
-      <div>jj</div>
+      <section className='container'>
+        <div className='project__item'>
+          <h2>Chat basico en socket io</h2>
+          <div className='project__item-dpn'>
+            <SiJavascript/>
+            <SiReact/>
+            <SiSocketdotio/>
+          </div>
+          <div className='project__item-gt'>
+            <BsGithub className='BsGithub' />
+            <a className='git_link' target='_blank' href='https://github.com/aamgituser/socket-chat'>https://github.com/aamgituser/socket-chat</a>
+          </div>
+          <iframe className='ytb-min'  src="https://www.youtube.com/embed/_R_yjcw7pk8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        
+
+
+      </section>
     </main>
   )
 }
